dc8332706424897362bb8b2317b56e2ff8a830f8
shithaarthan M <shithaarthan13@gmail.com>
2024-02-16 11:23:35 +0530

    final commit

--- gen_message.py
+++ gen_message.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
 def send_text_message():
-    account_sid = 'ACd1bfad39d541804c236ffad442873777'
-    auth_token = '5b534d415ecb17aa131ed150c6902d2e'
-    twilio_phone_number = '+13203329020'
-    recipient_phone_number = '+919965754127'
-    message_body = 'Hello from Python!'
+    account = 'your twilio account sid'
+    auth_token = 'your auth_token from twilio'
+    twilio_phone_number = 'your twilio phone number'
+    recipient_phone_number = 'reciver phone number'
+    message_body = 'Hello fire alert!'
     # Initialize Twilio client
-    client = Client(account_sid, auth_token)
+    client = Client(account, auth_token)
 
     # Send a text message
     message = client.messages.create(
@@ -19,5 +19,3 @@ def send_text_message():
     return message.sid
 
 
-message_sid = send_text_message()
-print("Message SID:", message_sid)
--- operate.py
+++ operate.py
@@ -1,7 +1,36 @@
 import main
 from os import system
+from ultralytics import YOLO
+import cv2
+import time
+import gen_message
+
+model = YOLO("path to your model file(yolo v8)")
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+count=0
 
 system('clear')
-val = int(input("send threat mail(0 for no/1 for yes) : "))
-if val == 1:
-   main.fireThreatMsg()
+
+while True:
+    _, frame = cap.read()
+    results=model.predict(frame)
+    for r in results:
+        img=r.plot()
+        for box in r.boxes:
+            
+            class_id = r.names[box.cls[0].item()]
+            cords = box.xyxy[0].tolist()
+            cords = [round(x) for x in cords]
+            confidence = round(box.conf[0].item(), 2)
+            if class_id=="fire":                   # change your class id here
+               main.fireThreatMsg()                #sends a email user
+               gen_message.send_text_message()     #sends text message to the user
+               time.sleep(300)
+                
+                
+            
+    cv2.imshow('Color Image', img)           #shows the output window for camera
+
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
